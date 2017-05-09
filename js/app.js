d02d25d5553282f48bb6c55a2b096a993b0ebb43
Dark Dragon <ulises5932@gmail.com>
2017-05-08 22:43:38 -0600

    commit.

--- js/app.js
+++ js/app.js
@@ -3,11 +3,6 @@ var App = angular.module('App', []);
 App.controller('TodoCtrl', function($scope, $http) {
   $http.get('list.json')
        .then(function(res){
-          $scope.todos = res.data;   
-          $scope.tod = [
-        {"titulo" : "El Juego de  Ender" ,"editorial" : "Edhasa"},
-        {"titulo" : "Alfreds Futterkiste", "editorial" : "Ediciones B / Zeta"},
-        {"titulo" : "Bon app", "editorial" : "Gigamesh"},
-    ];
+          $scope.todos = res.data; 
         });
 });
